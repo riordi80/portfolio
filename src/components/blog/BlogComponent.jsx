fe8de691241806972f75583737c0fb365c248364
riordi80 <riordi80@gmail.com>
2025-03-21 18:26:30 +0000

    Integrate RSS feed page

--- src/components/blog/BlogComponent.jsx
+++ src/components/blog/BlogComponent.jsx
@@ -6,7 +6,7 @@ function BlogComponent(props) {
   return (
     <div className="blog-posts">
       {posts.map((post) => (
-        <article className="blog-post" key={post.id}>
+        <article className="blog-post" key={post.id} id={`item-${post.id}`}>
           {post.image && (
             <div className="blog-post-image">
               <img src={post.image} alt={post.title} />
